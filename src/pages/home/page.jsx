21ee9f76629c649cc9f421a7365432558f4deb79
Nattaphan Pumipak <151860004+tik-04@users.noreply.github.com>
2025-07-28 10:07:13 +0700

    feat: Add new feature frontend about section

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,7 +1,10 @@
+import About from "./_components/about";
+
 export default function HomePage() {
   return (
     <div>
       <p className="text-5xl p-6">กำลังสร้างมองข้ามไปก่อน</p>
+      <About></About>
     </div>
   );
 }
