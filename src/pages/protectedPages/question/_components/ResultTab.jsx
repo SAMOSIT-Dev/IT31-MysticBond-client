122d765686ec5b247a259ec7be12d302cf0c1dc8
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-28 21:49:17 +0700

    feat: create question page and add personality functions

---
+++ src/pages/protectedPages/question/_components/ResultTab.jsx
@@ -0,0 +1,35 @@
+import { StepTabContent } from "../../../../components/Step";
+
+import BOOK_PROP from "../../../../assets/images/Prop2_3x.webp";
+import Button from "../../../../components/ui/Button";
+
+export default function ResultTab({ houseData = {} }) {
+  return (
+    <StepTabContent value="result">
+      <div className="flex flex-col">
+        {houseData?.title ? (
+          <div className="mx-auto text-center max-w-120">
+            <div className="mx-auto max-w-100 my-10">
+              <img src={houseData?.img ?? BOOK_PROP} alt="" />
+            </div>
+            <p className="text-5xl font-inria-serif my-10">{houseData.title}</p>
+            <p className="text-xl leading-normal">{houseData.description}</p>
+            <Button className="max-w-90 w-full mx-auto mt-10">เสร็จสิ้น</Button>
+          </div>
+        ) : (
+          <div className="mx-auto text-center">
+            <div className="mx-auto max-w-100 my-10">
+              <img src={BOOK_PROP} />
+            </div>
+            <p className="text-3xl leading-normal">โปรดรอการประกาศผลอีกครั้งในวันข้างหน้า</p>
+            <div className="mt-5 text-sm text-center text-[#A4A4A4]">
+              <p>“ทางเลือกวันนี้จะกำหนดตัวตนของคุณในวันข้างหน้า”</p>
+              <p className="leading-6">- Tim Fargo</p>
+            </div>
+            <Button className="max-w-90 w-full mx-auto mt-7.25">เสร็จสิ้น</Button>
+          </div>
+        )}
+      </div>
+    </StepTabContent>
+  );
+}
---
+++ src/pages/protectedPages/question/_components/StartTab.jsx
@@ -0,0 +1,30 @@
+import { StepTabContent, useStep } from "../../../../components/Step";
+import Button from "../../../../components/ui/Button";
+
+import SHIELD from "../../../../assets/images/shield_props.png";
+
+export default function StartTab() {
+  const { setValue, updateStepItemState } = useStep();
+  return (
+    <StepTabContent value="start">
+      <div className="flex flex-col">
+        <div className="relative mx-auto max-w-70 flex">
+          <img src={SHIELD} />
+        </div>
+        <p className="text-xl md:text-3xl text-center my-12 mt-0 max-w-200 mx-auto leading-normal md:leading-12">
+          โดยที่ประชาชนแห่งสหประชาชาติได้ยืนยันอีกครั้งไว้
+          ในกฎบัตรถึงศรัทธาในสิทธิมนุษยชนขั้นพื้นฐาน
+        </p>
+        <Button
+          className="max-w-90 w-full mx-auto"
+          onClick={() => {
+            setValue("question");
+            updateStepItemState("start", { disabled: true, activated: true });
+          }}
+        >
+          เริ่มทำแบบทดสอบ
+        </Button>
+      </div>
+    </StepTabContent>
+  );
+}
