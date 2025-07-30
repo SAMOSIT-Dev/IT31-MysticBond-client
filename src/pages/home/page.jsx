21d483214f826a597ed4e147e267a3aa0590b278
Nattaphan Pumipak <151860004+tik-04@users.noreply.github.com>
2025-07-31 00:21:27 +0700

    feat:Add new component frontend house section

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,9 +1,13 @@
 import Hero from "./_components/hero";
+import House from "./_components/house";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
+
+      
+      <House />
     </div>
   );
 }
