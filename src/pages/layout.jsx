67e672997ac4823dc0d0834801a7ae7645490cca
Sahapap <92364037+Sahapap1@users.noreply.github.com>
2025-07-30 18:16:38 +0700

    feat: Footer

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import ScrollToHash from "../components/ScrollToHash";
 
 export default function RootLayout() {
   return (
     <>
       <ScrollToHash />
-      <NavBar isRootPage />
+      {/* <NavBar isRootPage /> */}
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
